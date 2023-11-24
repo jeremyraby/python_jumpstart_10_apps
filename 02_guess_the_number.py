cfc824506fd17772632a2833300a18c3e66369e6
jeremyraby <jeremy.raby10@gmail.com>
2023-11-24 17:50:36 -0600

    Add list of numbers to choose from using random module

--- 02_guess_the_number.py
+++ 02_guess_the_number.py
@@ -1,3 +1,13 @@
+import random
+
 print('-' * 15)
 print('   GUESS THE NUMBER')
 print('-' * 15)
+
+secret_num = [1,2,3,4,5,6,7,8,9,10]
+
+message = f"I'm thinking of a number between 1 and 10. Can you guess it? "
+print(input(message))
+
+if message > random.choice(secret_num):
+    print("Sorry. That's too low.")
