50e1ea264c4c24771d4f92feeeb9756afcc8cb70
jeremyraby <jeremy.raby10@gmail.com>
2023-11-25 08:51:44 -0600

    Add 'else' statement

--- 02_guess_the_number.py
+++ 02_guess_the_number.py
@@ -4,11 +4,15 @@ print('-' * 15)
 print('   GUESS THE NUMBER')
 print('-' * 15)
 
-user_guess = input(f"I'm thinking of a number between 1 and 10. Can you guess it?")
+user_guess = int(input(f"I'm thinking of a number between 1 and 10. Can you guess it? "))
 
 number_choices = [1,2,3,4,5,6,7,8,9,10]
 secret_number = random.choice(number_choices)
 
 while user_guess != secret_number:
-    if user_guess > random.choice(secret_number):
-        print("Sorry. That's too low.")
+    if user_guess < secret_number:
+        print(f"Sorry, {user_guess} is too low. Try again.")
+    elif user_guess > secret_number:
+        print(f"Sorry, {user_guess} is too high. Try again.")
+    else:
+        print(f"Great guess! {user_guess} is right on the money!")
