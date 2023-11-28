4c11f1cf85fd79791644c2b73ca22b3c76fac204
jeremyraby <jeremy.raby10@gmail.com>
2023-11-28 09:08:18 -0600

    Change while loop to 'while True...' and move the guess prompt to inside the loop'

--- 02_guess_the_number.py
+++ 02_guess_the_number.py
@@ -6,12 +6,13 @@ print('-' * 18)
 
 secret_number = random.randint(1, 10)
 
-
-while user_guess != secret_number:
+while True:
     user_guess = int(input(f"I'm thinking of a number between 1 and 10. Can you guess it? "))
     if user_guess < secret_number:
         print(f"Sorry, {user_guess} is too low.")
+        continue
     elif user_guess > secret_number:
         print(f"Sorry, {user_guess} is too high.")
+        continue
     else:
         print(f"Great guess! {user_guess} is right on the money!")
