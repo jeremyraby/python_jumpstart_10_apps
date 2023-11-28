78eae7012b22281914cc4c7db6f7b6cc6189dcaf
jeremyraby <jeremy.raby10@gmail.com>
2023-11-28 09:05:48 -0600

    Change 'secret_number' to 'random.randint(1, 10)

--- 02_guess_the_number.py
+++ 02_guess_the_number.py
@@ -1,18 +1,17 @@
 import random
 
-print('-' * 15)
+print('-' * 18)
 print('   GUESS THE NUMBER')
-print('-' * 15)
+print('-' * 18)
 
-user_guess = int(input(f"I'm thinking of a number between 1 and 10. Can you guess it? "))
+secret_number = random.randint(1, 10)
 
-number_choices = [1,2,3,4,5,6,7,8,9,10]
-secret_number = random.choice(number_choices)
 
 while user_guess != secret_number:
+    user_guess = int(input(f"I'm thinking of a number between 1 and 10. Can you guess it? "))
     if user_guess < secret_number:
-        print(f"Sorry, {user_guess} is too low. Try again.")
+        print(f"Sorry, {user_guess} is too low.")
     elif user_guess > secret_number:
-        print(f"Sorry, {user_guess} is too high. Try again.")
+        print(f"Sorry, {user_guess} is too high.")
     else:
         print(f"Great guess! {user_guess} is right on the money!")
